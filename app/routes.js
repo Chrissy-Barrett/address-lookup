24d19c8141bd312895df478f12283cb1b93dab6f
Chrissy-Barrett <32103897+Chrissy-Barrett@users.noreply.github.com>
2025-11-24 21:11:20 +0000

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,50 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+const axios = require('axios'); 
+
+//Functions
+async function getGridReference(postcode) {
+  const url = `https://api.postcodes.io/postcodes/${postcode.replace(/\s/g, "")}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data.result;
+
+    if (data && data.eastings && data.northings) {
+    const   easting = data.eastings;
+    const   northing = data.northings;
+
+      console.log(`Postcode: ${postcode}`);
+      console.log(`Easting: ${easting}`);
+      console.log(`Northing: ${northing}`);
+
+  
+      return location = [ easting, northing ];
+  
+      
+    } else {
+      console.log(`Error: Could not find grid reference for postcode ${postcode}`);
+      return null;
+    }
+
+  } catch (error) {
+    console.error(`API Request Failed: ${error.message}`);
+    return null;
+  }
+}
+
+router.get('/location', function(req,res){
+    req.session.data = [{}]
+    res.render('location');
+});
+
+router.post('/location', function(req, res) {
+
+  getGridReference(req.session.data.postcode).then(data => {
+    req.session.data.location = location
+    res.redirect('/results'); 
+});
+
+      
+});
